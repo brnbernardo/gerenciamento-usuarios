78c5bdcdc02ce5808c70ad90f370569e557d70f5
Bruno Bernardo <brnbernardo@outlook.com>
2020-02-14 17:22:08 -0300

    Commit inicial

---
+++ gerenciamento-usuarios/src/test/java/br/com/sulamerica/gerenciamento/usuarios/GerenciamentoUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sulamerica.gerenciamento.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
